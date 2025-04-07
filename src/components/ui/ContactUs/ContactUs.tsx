af0e0327b1db81271efa8fe3efa1542ff1fd4f88
Konstantin <st4nshashkov@yandex.ru>
2025-04-07 13:23:31 +0300

    contact banner

--- src/components/ui/ContactUs/ContactUs.tsx
+++ src/components/ui/ContactUs/ContactUs.tsx
@@ -5,7 +5,7 @@ import { Link } from "react-router"
 const ContactUs = () => {
   return (
     <styled.Container>
-        <styled.Image src="/spec-tech-banner.jpg"/>
+        <styled.Image src="/gearup/spec-tech-banner.jpg"/>
         <styled.Heading>Press Contact Us to offer</styled.Heading>
         <Link to={'/contact'}>
           <Button size={{md:'4', sm:'3', initial:'2'}} style={{cursor:'pointer'}}>Contact us</Button>
