595080859bb49341fb0127650ff29808d8a6a6d6
Konstantin <st4nshashkov@yandex.ru>
2025-04-07 15:28:14 +0300

    fixed h3 mainpage

--- src/components/ui/ContactUs/ContactUs.tsx
+++ src/components/ui/ContactUs/ContactUs.tsx
@@ -6,7 +6,7 @@ const ContactUs = () => {
   return (
     <styled.Container>
         <styled.Image src="/gearup/spec-tech-banner.jpg" alt="special-working-machines-banner"/>
-        <styled.Heading>Press Contact Us to offer</styled.Heading>
+        <styled.Heading>Press Contact Us To Offer</styled.Heading>
         <Link to={'/contact'}>
           <Button size={{md:'4', sm:'3', initial:'2'}} style={{cursor:'pointer'}}>Contact us</Button>
         </Link>
